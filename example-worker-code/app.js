7cb74c0ad67774284fcb5af8d4d8ec4406f22a07
Franco Nahuel Rupp <ruppfn@gmail.com>
2022-09-29 01:10:33 -0300

    update worker code

--- example-worker-code/app.js
+++ example-worker-code/app.js
@@ -3,27 +3,31 @@ const express = require("express");
 
 const app = express();
 
-const router = express.Router();
+const pingRouter = express.Router();
 
-router.post("/", (req, res) => {
-    const message = "*** PONG ***";
-    console.log(message);
-    return res.status(200).json({ message });
+pingRouter.post("/", (req, res) => {
+    console.log("PING ENDPOINT");
+    return res.status(200).json({ message: 'ok' });
 })
 
-app.use("/ping", router);
+app.use("/ping", pingRouter);
+
+const scheduledRouter = express.Router();
+
+scheduledRouter.post("/", (req, res) => {
+    console.log("SCHEDULED ENDPOINT");
+    return res.status(200).json({ message: 'ok' });
+});
+
+app.use("/scheduled", scheduledRouter);
 
 app.post("/", (req, res, next) => {
-    console.log("\n--- BEGIN REQUEST ---");
-
     const headers = req.headers;
     const path = headers["x-aws-sqsd-attr-app-path"];
 
     req.url = path;
 
     app.handle(req, res, next);
-
-    console.log("--- END REQUEST ---\n");
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
